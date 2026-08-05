d00980663aa9418faea44fa355b3c875980e6919
Sebastian-5080 <jim.clark6365@web.de>
2026-08-05 15:01:51 +0000

    Day_01_Basic

---
+++ sommercamp/app.py
@@ -0,0 +1,96 @@
+# Hier importieren wir die benötigten Softwarebibliotheken.
+from os.path import abspath, exists
+from sys import argv
+from streamlit import (text_input, header, title, subheader,
+    container, markdown, link_button, divider, set_page_config)
+from pyterrier import IndexFactory
+from pyterrier.terrier import Retriever
+from pyterrier.text import get_text
+
+
+# Diese Funktion baut die App für die Suche im gegebenen Index auf.
+def app(index_dir) -> None:
+
+    # Konfiguriere den Titel der Web-App (wird im Browser-Tab angezeigt)
+    set_page_config(
+        page_title="Schul-Suchmaschine",
+        layout="centered",
+    )
+
+    # Gib der App einen Titel und eine Kurzbeschreibung:
+    title("Formel-Suchmaschine")
+    markdown("Hier kannst du unsere neue Formel-Suchmaschine nutzen:")
+
+    # Erstelle ein Text-Feld, mit dem die Suchanfrage (query)
+    # eingegeben werden kann.
+    query = text_input(
+        label="Suchanfrage",
+        placeholder="Suche...",
+        value="Formula 1",
+    )
+
+    # Wenn die Suchanfrage leer ist, dann kannst du nichts suchen.
+    if query == "":
+        markdown("Bitte gib eine Suchanfrage ein.")
+        return
+
+    # Öffne den Index.
+    index = IndexFactory.of(abspath(index_dir))
+    # Initialisiere den Such-Algorithmus
+    searcher = Retriever(
+        index,
+        wmodel="BM25",
+        num_results=10,
+    )
+    # Initialisiere das Modul, zum Abrufen der Texte.
+    text_getter = get_text(index, metadata=["url", "title", "text"])
+    # Baue die Such-Pipeline zusammen.
+    pipeline = searcher >> text_getter
+    # Führe die Such-Pipeline aus und suche nach der Suchanfrage.
+    results = pipeline.search(query)
+
+    # Zeige eine Unter-Überschrift vor den Suchergebnissen an.
+    divider()
+    header("Suchergebnisse")
+
+    # Wenn die Ergebnisliste leer ist, gib einen Hinweis aus.
+    if len(results) == 0:
+        markdown("Keine Suchergebnisse.")
+        return
+
+    # Wenn es Suchergebnisse gibt, dann zeige an, wie viele.
+    markdown(f"{len(results)} Suchergebnisse.")
+
+    # Gib nun der Reihe nach, alle Suchergebnisse aus.
+    for _, row in results.iterrows():
+        # Pro Suchergebnis, erstelle eine Box (container).
+        with container(border=True):
+            # Zeige den Titel der gefundenen Webseite an.
+            subheader(row["title"])
+            # Speichere den Text in einer Variablen (text).
+            text = row["text"]
+            # Schneide den Text nach 500 Zeichen ab.
+            text = text[:500]
+            # Ersetze Zeilenumbrüche durch Leerzeichen.
+            text = text.replace("\n", " ")
+            # Zeige den Dokument-Text an.
+            markdown(text)
+            # Gib Nutzern eine Schaltfläche, um die Seite zu öffnen.
+            link_button("Seite öffnen", url=row["url"])
+
+
+# Die Hauptfunktion, die beim Ausführen der Datei aufgerufen wird.
+def main():
+    # Lade den Pfad zum Index aus dem ersten Kommandozeilen-Argument.
+    index_dir = argv[1]
+
+    # Wenn es noch keinen Index gibt, kannst du die Suchmaschine nicht starten.
+    if not exists(index_dir):
+        exit(1)
+
+    # Rufe die App-Funktion von oben auf.
+    app(index_dir)
+
+
+if __name__ == "__main__":
+    main()
---
+++ sommercamp/searcher.py
@@ -0,0 +1,51 @@
+# Hier importieren wir die benötigten Softwarebibliotheken.
+from os.path import exists, abspath
+from sys import argv
+from pyterrier import IndexFactory
+from pyterrier.terrier import Retriever
+from pyterrier.text import get_text
+
+
+# In dieser Funktion öffnen wir den Index und suchen darin
+# nach der gegebenen Suchanfrage ("query").
+def search(index_dir, query):
+    # Öffne den Index.
+    index = IndexFactory.of(abspath(index_dir))
+    # Initialisiere den Such-Algorithmus
+    searcher = Retriever(
+        index,
+        # Der bekannteste Suchalgorithmus heißt "BM25".
+        wmodel="BM25",
+        # Und es sollen bis zu 10 Ergebnisse zurückgegeben werden.
+        num_results=10,
+    )
+    # Initialisiere ein Modul, was den Text
+    # der gefundenen Dokumente aus dem Index lädt.
+    text_getter = get_text(index, metadata=["url", "title", "text"])
+    # Baue nun die "Pipeline" für die Suche zusammen
+    # Zuerst suchen, dann Text abrufen.
+    pipeline = searcher >> text_getter
+    # Führe die Such-Pipeline aus und suche nach der Suchanfrage (query).
+    results = pipeline.search(query)
+    return results
+
+
+# Die Hauptfunktion, die beim Ausführen der Datei aufgerufen wird.
+def main():
+    # Lade den Pfad zum Index aus dem ersten Kommandozeilen-Argument.
+    index_dir = argv[1]
+    # Lade die Suchanfrage aus dem zweiten Kommandozeilen-Argument.
+    query = argv[2]
+
+    # Wenn es noch keinen Index gibt, können wir nichts zurückgeben.
+    if not exists(index_dir):
+        exit(1)
+
+    print("Searching...")
+    # Rufe die Such-Funktion von oben auf.
+    results = search(index_dir, query)
+    # Gib die Suchergebnisse im Terminal aus.
+    print(results)
+
+if __name__ == "__main__":
+    main()
