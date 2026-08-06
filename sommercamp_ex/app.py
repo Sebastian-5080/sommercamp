2f23f125ad69eb534eed7a34db870de8add3e225
Sebastian-5080 <jim.clark6365@web.de>
2026-08-06 14:36:22 +0000

    09_02_neuro_fin

--- sommercamp_ex/app.py
+++ sommercamp_ex/app.py
@@ -1,6 +1,7 @@
 # Hier importieren wir die benötigten Softwarebibliotheken.
 from os.path import abspath, exists
 from sys import argv
+import streamlit as st
 from streamlit import (text_input, header, title, subheader, container, 
                        markdown, link_button, divider, set_page_config)
 from pyterrier import IndexFactory
@@ -16,7 +17,7 @@ def app(index_dir) -> None:
 
     # Konfiguriere den Titel der Web-App (wird im Browser-Tab angezeigt)
     set_page_config(
-        page_title="Schul-Suchmaschine",
+        page_title="Sim Racing-Suchmaschine",
         layout="centered",
     )
 
@@ -32,57 +33,104 @@ def app(index_dir) -> None:
         value="",
     )
 
+    on = st.toggle("Activate AI ✨")
+
     # Wenn die Suchanfrage leer ist, dann kannst du nichts suchen.
     if query == "":
         markdown("Bitte gib eine Suchanfrage ein.")
         return
+    
+    elif on:
+        # Öffne den Index.
+        index = IndexFactory.of(abspath(index_dir))
+        # Initialisiere den Such-Algorithmus
+        searcher = Retriever(
+            index,
+            wmodel="BM25",
+            num_results=10,
+        )
+        # Initialisiere das Modul, zum Abrufen der Texte.
+        text_getter = get_text(index, metadata=["url", "title", "text"])
+        # Dense retriver Model laden
+        model = pyterrier_dr.SBertBiEncoder('sentence-transformers/all-MiniLM-L6-v2') 
+        # Baue die Such-Pipeline zusammen.
+        pipeline = searcher >> text_getter >> model.scorer()
 
-    # Öffne den Index.
-    index = IndexFactory.of(abspath(index_dir))
-    # Initialisiere den Such-Algorithmus
-    searcher = Retriever(
-        index,
-        wmodel="BM25",
-        num_results=10,
-    )
-    # Initialisiere das Modul, zum Abrufen der Texte.
-    text_getter = get_text(index, metadata=["url", "title", "text"])
-    # Dense retriver Model laden
-    model = pyterrier_dr.SBertBiEncoder('sentence-transformers/all-MiniLM-L6-v2') 
-    # Baue die Such-Pipeline zusammen.
-    pipeline = searcher >> text_getter >> model.scorer()
+        # Führe die Such-Pipeline aus und suche nach der Suchanfrage.
+        results = pipeline.search(query)
 
-    # Führe die Such-Pipeline aus und suche nach der Suchanfrage.
-    results = pipeline.search(query)
+        # Zeige eine Unter-Überschrift vor den Suchergebnissen an.
+        divider()
+        header("Suchergebnisse")
 
-    # Zeige eine Unter-Überschrift vor den Suchergebnissen an.
-    divider()
-    header("Suchergebnisse")
+        # Wenn die Ergebnisliste leer ist, gib einen Hinweis aus.
+        if len(results) == 0:
+            markdown("Keine Suchergebnisse.")
+            return
 
-    # Wenn die Ergebnisliste leer ist, gib einen Hinweis aus.
-    if len(results) == 0:
-        markdown("Keine Suchergebnisse.")
-        return
+        # Wenn es Suchergebnisse gibt, dann zeige an, wie viele.
+        markdown(f"{len(results)} Suchergebnisse.")
 
-    # Wenn es Suchergebnisse gibt, dann zeige an, wie viele.
-    markdown(f"{len(results)} Suchergebnisse.")
+        # Gib nun der Reihe nach, alle Suchergebnisse aus.
+        for _, row in results.iterrows():
+            # Pro Suchergebnis, erstelle eine Box (container).
+            with container(border=True):
+                # Zeige den Titel der gefundenen Webseite an.
+                subheader(row["title"])
+                # Speichere den Text in einer Variablen (text).
+                text = row["text"]
+                # Schneide den Text nach 500 Zeichen ab.
+                text = text[:500]
+                # Ersetze Zeilenumbrüche durch Leerzeichen.
+                text = text.replace("\n", " ")
+                # Zeige den Dokument-Text an.
+                markdown(text)
+                # Gib Nutzern eine Schaltfläche, um die Seite zu öffnen.
+                link_button("Seite öffnen", url=row["url"])
+    else:
+        # Öffne den Index.
+        index = IndexFactory.of(abspath(index_dir))
+        # Initialisiere den Such-Algorithmus. 
+        searcher = Retriever(
+            index,
+            wmodel="BM25",
+            num_results=10,
+        )
+        # Initialisiere das Modul, zum Abrufen der Texte.
+        text_getter = get_text(index, metadata=["url", "title", "text"])
+        # Baue die Such-Pipeline zusammen.
+        pipeline = searcher >> text_getter
+        # Führe die Such-Pipeline aus und suche nach der Suchanfrage.
+        results = pipeline.search(query)
 
-    # Gib nun der Reihe nach, alle Suchergebnisse aus.
-    for _, row in results.iterrows():
-        # Pro Suchergebnis, erstelle eine Box (container).
-        with container(border=True):
-            # Zeige den Titel der gefundenen Webseite an.
-            subheader(row["title"])
-            # Speichere den Text in einer Variablen (text).
-            text = row["text"]
-            # Schneide den Text nach 500 Zeichen ab.
-            text = text[:500]
-            # Ersetze Zeilenumbrüche durch Leerzeichen.
-            text = text.replace("\n", " ")
-            # Zeige den Dokument-Text an.
-            markdown(text)
-            # Gib Nutzern eine Schaltfläche, um die Seite zu öffnen.
-            link_button("Seite öffnen", url=row["url"])
+        # Zeige eine Unter-Überschrift vor den Suchergebnissen an.
+        divider()
+        header("Suchergebnisse")
+
+        # Wenn die Ergebnisliste leer ist, gib einen Hinweis aus.
+        if len(results) == 0:
+            markdown("Keine Suchergebnisse.")
+            return
+
+        # Wenn es Suchergebnisse gibt, dann zeige an, wie viele.
+        markdown(f"{len(results)} Suchergebnisse.")
+
+        # Gib nun der Reihe nach, alle Suchergebnisse aus.
+        for _, row in results.iterrows():
+            # Pro Suchergebnis, erstelle eine Box (container).
+            with container(border=True):
+                # Zeige den Titel der gefundenen Webseite an.
+                subheader(row["title"])
+                # Speichere den Text in einer Variablen (text).
+                text = row["text"]
+                # Schneide den Text nach 500 Zeichen ab.
+                text = text[:500]
+                # Ersetze Zeilenumbrüche durch Leerzeichen.
+                text = text.replace("\n", " ")
+                # Zeige den Dokument-Text an.
+                markdown(text)
+                # Gib Nutzern eine Schaltfläche, um die Seite zu öffnen.
+                link_button("Seite öffnen", url=row["url"])
 
 
 # Die Hauptfunktion, die beim Ausführen der Datei aufgerufen wird.
